e3b9fd57f946887068b59fefa9b36027fb9e8ff8
JessieBai <jessiebai90@gmail.com>
2023-01-22 18:39:53 +1300

    remove swagger setting

--- WebHost/Program.cs
+++ WebHost/Program.cs
@@ -19,13 +19,6 @@ builder.Services.AddDomainServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
