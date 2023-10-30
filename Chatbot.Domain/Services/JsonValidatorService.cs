572a6e6385740d4ce20202350763559744da61dd
zh ni <792293993@qq.com>
2023-10-30 22:07:43 +0800

    Validate json format

---
+++ Chatbot.Domain/Services/JsonValidatorService.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Chatbot.Domain.Services
+{
+    public class JsonValidatorService
+    {
+        public bool IsValidJson(string jsonString)
+        {
+            try
+            {
+                JToken.Parse(jsonString);
+                return true;
+            }
+            catch (JsonReaderException)
+            {
+                return false;
+            }
+        }
+    }
+}
